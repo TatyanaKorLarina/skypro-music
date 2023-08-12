e9a13557f2bf430e2faa35a82380c3e856f9b080
TaTia <tatyanakoryachkina@yandex.ru>
2023-08-12 18:47:50 +0300

    created Sidebar component

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AudioPlayer from './components/audioPlayer/AudioPlayer.js';
 import NavMenu from './components/navMenu/NavMenu.js';
 import Tracklist from './components/tracklist/Tracklist.js';
+import Sidebar from './components/sidebar/Sidebar.js';
 import './App.css';
 
 function App() {
@@ -12,47 +13,7 @@ function App() {
         <main className="main">
         <NavMenu />
         <Tracklist />
-          <div className="main__sidebar sidebar">
-            <div className="sidebar__personal">
-              <p className="sidebar__personal-name">Sergey.Ivanov</p>
-              <div className="sidebar__icon">
-                <svg alt="logout">
-                  <use xlinkHref="img/icon/sprite.svg#logout"></use>
-                </svg>
-              </div>
-            </div>
-            <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <Sidebar />
         </main>
         <AudioPlayer />
         <footer className="footer"></footer>
