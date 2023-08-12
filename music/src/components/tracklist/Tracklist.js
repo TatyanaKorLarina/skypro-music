f143c132be7b6b20250b7a2dfb089d0f7800e4e5
TaTia <tatyanakoryachkina@yandex.ru>
2023-08-12 22:44:53 +0300

    did 2nd hw

--- music/src/components/tracklist/Tracklist.js
+++ music/src/components/tracklist/Tracklist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Tracklist.css';
 import Track from '../track/Track.js';
+import Filter from '../filter/Filter.js';
 
 const Tracklist = () => {
   return (
@@ -17,16 +18,7 @@ const Tracklist = () => {
       />
     </div>
     <h2 className="centerblock__h2">Треки</h2>
-    <div className="centerblock__filter filter">
-      <div className="filter__title">Искать по:</div>
-      <div className="filter__button button-author _btn-text">
-        исполнителю
-      </div>
-      <div className="filter__button button-year _btn-text">
-        году выпуска
-      </div>
-      <div className="filter__button button-genre _btn-text">жанру</div>
-    </div>
+    <Filter />
     <div className="centerblock__content">
       <div className="content__title playlist-title">
         <div className="playlist-title__col col01">Трек</div>
