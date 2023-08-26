82d8f4369b3c2ab9531fb0696706256f0905aa76
TaTia <tatyanakoryachkina@yandex.ru>
2023-08-26 13:03:44 +0300

    SidebarStyledComponents

--- music/src/components/sidebar/Sidebar.js
+++ music/src/components/sidebar/Sidebar.js
@@ -1,49 +1,16 @@
 
-import './Sidebar.css';
+
 
 import Playlist from '../playlist/Playlist';
 
-import  styled  from 'styled-components'
-
-const StyledMainSidebar = styled.div`
-  max-width: 418px;
-  padding: 20px 90px 20px 78px;
-`
-
-const StyledSidebarBlock = styled.div`
-  height: 100%;
-  padding: 40px 0 0 0;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
-  flex-direction: column;
-  -webkit-box-pack: start;
-  -ms-flex-pack: start;
-  justify-content: flex-start;
-`
-
-const StyledSidebarList = styled.div`
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
-  flex-direction: column;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
-  align-items: center;
-`
+import * as S from './Sidebar.styles'
 
 function Sidebar() {
   return (
-    <StyledMainSidebar>
+    <S.MainSidebar>
             
-            <StyledSidebarBlock>
-              <StyledSidebarList>
+            <S.SidebarBlock>
+              <S.SidebarList>
               <Playlist
         imgUrl="img/playlist01.png"
         
@@ -54,9 +21,9 @@ function Sidebar() {
       <Playlist
         imgUrl="img/playlist03.png"
       />
-              </StyledSidebarList>
-            </StyledSidebarBlock>
-          </StyledMainSidebar>
+              </S.SidebarList>
+            </S.SidebarBlock>
+          </S.MainSidebar>
   );
 }
 
