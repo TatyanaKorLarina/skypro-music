f46beab5639dd41d68911c14a54e03e6f81e6cc6
TaTia <tatyanakoryachkina@yandex.ru>
2023-09-23 22:48:00 +0300

    loop

--- music/src/components/audioPlayer/AudioPlayer.js
+++ music/src/components/audioPlayer/AudioPlayer.js
@@ -9,6 +9,7 @@ export default function AudioPlayer({ currentTrack }) {
     const [currentTime, setCurrentTime] = useState(0);
     const [currentDuration, setCurrentDuration] = useState(0);
     const [volume, setVolume] = useState(60);
+    const [repeat, setRepeat] = useState(false);
     const audioRef = useRef(null);
     const progressBarRef = useRef(null);
 
@@ -19,13 +20,23 @@ export default function AudioPlayer({ currentTrack }) {
     }, [volume, audioRef]);
 
   const handleStart = () => {
-    audioRef.current.play();
-    setIsPlaying(true);
+    if (audioRef.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
   };
 
   const handleStop = () => {
     audioRef.current.pause();
     setIsPlaying(false);
+  };
+
+
+  const handleRepeat = () => {
+    if (audioRef.current) {
+      audioRef.current.loop = !repeat;
+      setRepeat(!repeat);
+    }
   };
 
   const togglePlay = isPlaying ? handleStop : handleStart;
@@ -84,7 +95,8 @@ export default function AudioPlayer({ currentTrack }) {
             </S.BarPlayerProgress> 
             <S.BarPlayerBlock>
               <S.BarPlayer>
-                <S.PlayerControls isPlaying={isPlaying} currentTrack={currentTrack}
+                <S.PlayerControls isPlaying={isPlaying} currentTrack={currentTrack} handleRepeat={handleRepeat}
+                repeat={repeat}
                 togglePlay={togglePlay}>
                   <S.PlayerBtnPrev>
                     <S.PlayerBtnPrevSvg alt="prev">
@@ -106,8 +118,27 @@ export default function AudioPlayer({ currentTrack }) {
                     </S.PlayerBtnNextSvg>
                   </S.PlayerBtnNext>
                   <S.PlayerBtnRepeat>
-                    <S.PlayerBtnRepeatSvg alt="repeat">
-                      <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
+                    <S.PlayerBtnRepeatSvg onClick={handleRepeat} alt="repeat">
+                    {repeat ? (
+            <svg
+              width="15"
+              height="13"
+              viewBox="0 0 20 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 3L5 0.113249V5.88675L10 3ZM7 14.5C3.96243 14.5 1.5 12.0376 1.5 9H0.5C0.5 12.5899 3.41015 15.5 7 15.5V14.5ZM1.5 9C1.5 5.96243 3.96243 3.5 7 3.5V2.5C3.41015 2.5 0.5 5.41015 0.5 9H1.5Z"
+                fill="white"
+              />
+              <path
+                d="M10 15L15 17.8868V12.1132L10 15ZM13 3.5C16.0376 3.5 18.5 5.96243 18.5 9H19.5C19.5 5.41015 16.5899 2.5 13 2.5V3.5ZM18.5 9C18.5 12.0376 16.0376 14.5 13 14.5V15.5C16.5899 15.5 19.5 12.5899 19.5 9H18.5Z"
+                fill="white"
+              />
+            </svg>
+          ) : (
+            <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+          )}
                     </S.PlayerBtnRepeatSvg>
                   </S.PlayerBtnRepeat>
                   <S.PlayerBtnShuffle>
