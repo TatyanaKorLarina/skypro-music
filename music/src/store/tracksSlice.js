212dc365efff43d143fcfc28817a61b81e0a805f
TaTia <tatyanakoryachkina@yandex.ru>
2023-10-22 17:44:46 +0300

    added reducers

--- music/src/store/tracksSlice.js
+++ music/src/store/tracksSlice.js
@@ -1,25 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const tracksSlice = createSlice({
-  name: "trcks",
+  name: "tracks",
   initialState: {
     
-    trck: [],
+    track: null,
     tracklist: [],
+    isPlaying: false,
+    isShuffleEnabled: false,
+    currentPlaylist: null,
   },
   reducers: {
     setCurrentAudio(state, action) {
-      state.trck[0] = {
-        trackData: action.payload,
-      }  
+      state.track =  action.payload;
+      
     },
+
+
+    setPlayingStatus: (state, action) => {
+      state.isPlaying = action.payload;
+    },
+    setShuffleStatus: (state, action) => {
+      state.isShuffleEnabled = action.payload;
+    },
+   
+
     setTracklist(state, action) {
       state.tracklist = action.payload
     },
+
   },
   
 });
 
-export const { setCurrentAudio, setTracklist, } = tracksSlice.actions;
+export const { setCurrentAudio, setPlayingStatus,
+  setShuffleStatus, setTracklist, } = tracksSlice.actions;
 //export const { nextTack, previousTrack } = tracksSlice.actions
 export default tracksSlice.reducer;
